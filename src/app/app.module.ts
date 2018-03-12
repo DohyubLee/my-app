cfb3e947b3319db421d72eb1947c2a521439d49f
DohyubLee <hani7304@gmail.com>
2018-03-12 20:03:56 +0900

    component 생성

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { BoardListComponent } from './board-list/board-list.component';
+import { BoardDetailComponent } from './board-detail/board-detail.component';
+import { BoardInsertComponent } from './board-insert/board-insert.component';
+import { BoardEditComponent } from './board-edit/board-edit.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    BoardListComponent,
+    BoardDetailComponent,
+    BoardInsertComponent,
+    BoardEditComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
