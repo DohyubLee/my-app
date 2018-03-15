25e9b7202a187a8311f2996e3bdf7aa46792850b
DohyubLee <hani7304@gmail.com>
2018-03-15 16:34:01 +0900

    추가 라우트 구성

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { BoardListComponent } from './board-list/board-list.component';
+import { BoardDetailComponent } from './board-detail/board-detail.component';
+import { BoardInsertComponent } from './board-insert/board-insert.component';
+import { BoardEditComponent } from './board-edit/board-edit.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'board-list', component: BoardListComponent },
+  { path: 'board-detail', component: BoardDetailComponent },
+  { path: 'board-insert', component: BoardInsertComponent },
+  { path: 'board-edit', component: BoardEditComponent }
 ];
 
 @NgModule({
