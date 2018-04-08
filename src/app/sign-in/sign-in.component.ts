d1eb237705cb634ec36951fba633247c1a2608d6
DohyubLee <hani7304@gmail.com>
2018-04-09 05:49:48 +0900

    세션 ?

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  user: any = {};
+  url = 'http://localhost:3000/login';
+  constructor(
+    private http: HttpClient,
+    private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.http.post(this.url, this.user).subscribe(res => {
+      if (res.status === "login") {
+        this.router.navigate(['board-list']);  //페이지 이동
+      }
+   })
   }
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,16 +9,36 @@ import { Router } from '@angular/router';
 })
 export class SignUpComponent implements OnInit {
   user: any = {};
-  constructor(private router: Router) { }
+  url = 'http://localhost:3000/register';
+  constructor(
+    private http: HttpClient, 
+    private router: Router) { }
   
   ngOnInit() {
   }
   
   onSubmit() {
+    if (!this.user.name) {
+      console.log('The name is empty.')
+      return;
+    }
+    if (!this.user.password) {
+      console.log('The password is empty.')
+      return;
+    }
+    if (!this.user.email) {
+      console.log('The email is empty.')
+      return;
+    }
+    this.http.post(this.url, this.user).subscribe(res => {
+      if (res.status === true) {
+        this.router.navigate(['sign-in']);  //페이지 이동
+      }
+    })
     console.log('hi');
     console.log('email:',this.user.email);
     console.log('user',this.user.name);
     console.log('password',this.user.password);
-    // this.router.navigate(['sign-in']);  //페이지 이동
+   
   }
 }
